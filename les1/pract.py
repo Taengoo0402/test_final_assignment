e00f72a42d2107d1a14eeafb054127db3d1c829b
Taengoo0402 <niro.kohnesheen@student.hu.nl>
2017-09-12 11:49:29 +0200

    test final assignment

---
+++ les1/pract.py
@@ -0,0 +1,66 @@
+#1.2
+len ('Supercalifragilisticexpialidocious')
+34
+#b
+len ('Antidisestablishmentarianism')
+28
+len ('Honorificabilitudinitatibus')
+27
+#ja
+#bartok en buxtehude
+#1.3
+a = 6
+b = 7
+c= (a + b)/ 2
+inventaris = ('papier', 'nietje', 'pennen')
+MyName = 'Nir0
+  File "<input>", line 1
+    MyName = 'Nir0
+                 ^
+SyntaxError: EOL while scanning string literal
+Myname= Niro
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+NameError: name 'Niro' is not defined
+Myname = 'Niro'
+LastName = 'Kohnesheen'
+'Myname'+ 'LastName'
+'MynameLastName'
+Myname + '' + LastName
+'NiroKohnesheen'
+#1.4
+a < 6.75 < b
+True
+inventaris > Myname
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+TypeError: '>' not supported between instances of 'tuple' and 'str'
+#1.5
+favorieten ['Gdragon']
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+NameError: name 'favorieten' is not defined
+favorieten = ['Gdragon']
+favorieten.append(Taeyang)
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+NameError: name 'Taeyang' is not defined
+favorieten.append('Taeyang')
+favorieten
+['Gdragon', 'Taeyang']
+favorieten.pop ('Taeyang')
+Traceback (most recent call last):
+  File "<input>", line 1, in <module>
+TypeError: 'str' object cannot be interpreted as an integer
+favorieten.pop
+<built-in method pop of list object at 0x03FCED50>
+favorieten
+['Gdragon', 'Taeyang']
+favorieten.remove('Taeyang')
+favorieten
+['Gdragon']
+favorieten.append('Taeyeon')
+favorieten
+['Gdragon', 'Taeyeon']
+
+

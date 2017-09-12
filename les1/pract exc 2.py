ef6e1801f4c4c7b5e428e0616038ba5a253564d5
Taengoo0402 <niro.kohnesheen@student.hu.nl>
2017-09-12 14:20:20 +0200

    test final assignment

--- les1/pract exc 2.py
+++ les1/pract exc 2.py
@@ -14,3 +14,21 @@ print(gemiddelde)
 print(beloning)
 #2.3
 0 == 1 == 2
+0==(1== 2)
+print(0==(1==2))
+2 + 3 == 4 + 5 == 7
+2 + 3 == 4 + 5 == 7
+print(2 + 3 == 4 + 5 == 7)
+1 < -1 == 3 > 4
+(1 < -1) == (3 > 4 )
+print((1 < -1) == (3 > 4 ))
+#2.4
+uurloon= 6
+aantal_uur= 8
+salaris= uurloon * aantal_uur
+'Wat verdien je per uur?' + str(uurloon)
+'Hoeveel heb je gewerkt?'+ str(aantal_uur)
+print ('Wat verdien je per uur?' + str(uurloon))
+print ('Hoeveel heb je gewerkt?'+ str(aantal_uur))
+str(aantal_uur)+ 'werken levert'+ str(salaris)+ 'euro op.'
+print(str(aantal_uur)+ 'werken levert'+ str(salaris)+ 'euro op.')

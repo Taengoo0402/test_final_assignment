8dc7a5dff5a82e32692d2596afdd2a35037b3dd5
Taengoo0402 <niro.kohnesheen@student.hu.nl>
2017-09-12 13:28:28 +0200

    test final assignment

---
+++ les1/pract exc 2.py
@@ -0,0 +1,16 @@
+#2.1
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+list = (letters.count('A'), letters.count('B'), letters.count('C'))
+print(list)
+#2.2
+cijfICOR= 7
+cijfPROG= 7
+cijfCSN= 7
+gemiddelde= (cijfICOR + cijfPROG + cijfCSN)/ 3
+beloning= (gemiddelde * 3)* 30
+overzicht= 'Mijn cijfers (gemiddeld een '+ str(gemiddelde)+ ')leveren een beloning van ' + str(beloning)+ 'op'
+print(overzicht)
+print(gemiddelde)
+print(beloning)
+#2.3
+0 == 1 == 2

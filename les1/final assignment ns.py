4b27c33892ec744fe275cfedb068b4e58aa78120
Taengoo0402 <niro.kohnesheen@student.hu.nl>
2017-09-19 13:54:27 +0200

    test final assignment

--- les1/final assignment ns.py
+++ les1/final assignment ns.py
@@ -4,33 +4,33 @@ def standaardtarief(km):
         prijs = 0
     else:
         if km >=50:
-            prijs = 15 + 0,60 * (km)
+            prijs = 15 + 0.60 * (km)
         else:
-            prijs = 0,80 * (km)
+            prijs = 0.80 * (km)
     return prijs
 
 #deel 2
 def ritprijs (leeftijd, weekendrit, afstandKM):
     prijs= standaardtarief(afstandKM)
     RitPrijs= 0
-    if weekendrit == False:
+    if not weekendrit:
         if leeftijd <12 or leeftijd >=65:
-         RitPrijs = prijs * 0,70
+            RitPrijs = prijs * 0.70
 
-    if weekendrit == True:
+    if weekendrit:
         if leeftijd <12 or leeftijd >=65:
-            RitPrijs= prijs * 0,65
+            RitPrijs= prijs * 0.65
         else:
-            RitPrijs= prijs * 0,60
+            RitPrijs= prijs * 0.60
     return RitPrijs
 
-leeftijd= eval(input('Wat is je leeftijd?'))
-weekendrit= input('Reis je in het weekend?')
+leeftijd= eval(input('Wat is je leeftijd?\n'))
+weekendrit= input('Reis je in het weekend?\n')
 if weekendrit == 'Ja':
-    weekendrit == True
+    weekendrit = True
 else:
-    weekendrit == False
-afstandKM= eval(input('Hoever reis je?'))
+    weekendrit = False
+afstandKM= eval(input('Hoever reis je?\n'))
 print(ritprijs(leeftijd, weekendrit, afstandKM))
 
 
